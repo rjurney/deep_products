af995c869c27e3a6d02a77e7aaceaf8b46a9bbd2
Russell Jurney <russell.jurney@gmail.com>
2019-07-30 13:45:35 -0700

    In progress work preparing a superdoc

--- code/stackoverflow/prepare_docs.spark.py
+++ code/stackoverflow/prepare_docs.spark.py
@@ -1,6 +1,6 @@
 from frozendict import frozendict
 import json
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 
 spark = SparkSession.builder.appName('Deep Products - Convert XML to Parquet').getOrCreate()
 sc = spark.sparkContext
@@ -21,7 +21,7 @@ print([x['_PostId'] for x in comments_df.orderBy('_PostId').take(10)])
 
 
 # Join and evaluate
-post_comments = posts_df.join(comments_df, posts_df._Id == comments_df._PostId)
+post_comments = posts_df.join(comments_df, posts_df._Id == comments_df._PostId, how='left_outer')
 print('Post comments count: {:,}'.format(post_comments.count()))
 
 comment_columns = [col for col in comments_df.columns]
@@ -45,11 +45,80 @@ def compose_dict(x):
     post.update({'Comments': comments})
     return post
 
+
 combined_docs = post_comment_dicts.reduceByKey(lambda a, b: a + b).map(compose_dict)
 print('Combined docs count: {:,}'.format(combined_docs.count()))
 
-os.system('rm -rf data/stackoverflow/sample/combined/Posts.Comments.jsonl.gz')
+os.system('aws s3 rm --recursive s3://stackoverflow-events/06-24-2019/combined/Posts.Comments.jsonl.gz')
 combined_docs.map(lambda x: json.dumps(x)).coalesce(1).saveAsTextFile(
-    path='data/stackoverflow/sample/combined/Posts.Comments.jsonl.gz',
+    path='s3://stackoverflow-events/06-24-2019/combined/Posts.Comments.jsonl.gz',
     compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec"
 )
+
+print('Original docs: {:,}'.format(combined_docs.count()))
+print('Docs with _OwnerUserId: {:,}'.format(
+    combined_docs.filter(lambda x: '_OwnerUserId' in x and x['_OwnerUserId'] is not None).count())
+)
+
+filtered_docs = combined_docs.filter(lambda x: '_OwnerUserId' in x and x['_OwnerUserId'] is not None)
+print('Step 1 docs: {:,}'.format(filtered_docs.count()))
+
+joinable_docs = filtered_docs.map(lambda x: (x['_OwnerUserId'], x))\
+                             .filter(lambda x: x[0] is not None)
+print('Step 2 docs: {:,}'.format(joinable_docs.count()))
+
+users_df = spark.read.json('s3://stackoverflow-events/06-24-2019/Users.100K.Questions.jsonl.gz')
+users = users_df.rdd.filter(lambda x: x['_Id'] is not None)
+print('Original docs: {:,}'.format(users_df.count()))
+print('With ID: {:,}'.format(users.count()))
+
+def choose_list(x):
+    if not x or len(x) < 1:
+        print("E")
+        return []
+    return x
+
+users_docs = joinable_docs.leftOuterJoin(
+    users.filter(lambda x: '_Id' in x and x['_Id'] is not None).map(lambda x: (x['_Id'], x))
+).map(lambda x: x[1]).map(lambda x: (x[0], x[1]))
+print('user_docs: {:,}'.format(users_docs.count()))
+
+def update_owner(x):
+    post = x[0]
+    users = x[1]
+    if users is None or users == '':
+        users = None
+
+    if isinstance(users, Row):
+        users = users.asDict()
+
+    if isinstance(users, str):
+        users = None
+
+    post.update({'_Owner': users})
+    return post
+updated_users_docs = users_docs.map(update_owner)
+print('Total updated_users_docs: {:,}'.format(updated_users_docs.count()))
+
+joinable_docs = updated_users_docs.map(lambda x: (x['_Id'], x))
+
+votes_df = spark.read.json('s3://stackoverflow-events/06-24-2019/Votes.100K.Questions.jsonl.gz')
+joinable_votes = votes_df.rdd.filter(lambda x: '_PostId' in x and x['_PostId'] is not None).map(lambda x: (x['_PostId'], x))
+joinable_votes.count()
+
+vote_groups = votes_df.rdd.groupBy(lambda x: x['_PostId']).map(lambda x: (x[0], [y.asDict() for y in x[1]]))
+
+docs_votes_joined = joinable_docs.leftOuterJoin(vote_groups).map(lambda x: x[1])
+
+def update_vote(x):
+    post = x[0]
+    votes = x[1]
+
+    post.update({'_Votes': votes})
+    return post
+docs_votes = docs_votes_joined.map(update_vote)
+print('docs_votes count: {:,}'.format(docs_votes.count()))
+
+
+os.system('aws s3 rm --recursive s3://stackoverflow-events/06-24-2019/CombinedDocs.100K.Questions.jsonl.gz')
+docs_votes.
